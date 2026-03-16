fd41ca12b227cba88c3d6e3d2415fffe1e9b8a35
Elodie Mesiha <mesiha@sheridancollege.ca>
2026-03-16 16:11:41 -0400

    first page layout complete

--- App.tsx
+++ App.tsx
@@ -6,65 +6,74 @@
  */
 
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 function App() {
   return (
-    <View style={styles.container}>
-  <Text style={styles.title}>Assignment 2</Text>
-  <Text style={styles.subtitle}>Random Number Generator</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Random Number Generator</Text>
+      </View>
 
-  <View style={styles.numberBox}>
-    <Text style={styles.numberText}>...</Text>
-  </View>
+      <View style={styles.content}>
+        <Text style={styles.numberText}>...</Text>
+      </View>
 
-  <TouchableOpacity style={styles.button}>
-    <Text style={styles.buttonText}>Generate</Text>
-  </TouchableOpacity>
-</View>
+      <View style={styles.buttonRow}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Generate</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>View Statistics</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#b08968',
+    paddingTop: 50,
+  },
+  header: {
+    backgroundColor: '#8b5e3c',
+    paddingVertical: 14,
+    paddingHorizontal: 14,
+  },
+
+  headerTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  
+  content: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f7f7f7',
-    padding: 20,
   },
 
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-
-  subtitle: {
-    fontSize: 18,
-    marginTop: 10,
-    textAlign: 'center',
-  },
-  numberBox: {
-  width: 180,
-  height: 180,
-  borderRadius: 20,
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginTop: 30,
-},
 numberText: {
   fontSize: 60,
   fontWeight: 'bold',
-  color: '#7f5539',
+  color: 'white',
 },
+buttonRow: {
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    paddingBottom: 18,
+    gap: 12,
+  },
+
 button: {
-  backgroundColor: '#b08968',
+  flex: 1,
+  backgroundColor: '#7f5539',
   paddingVertical: 14,
-  paddingHorizontal: 30,
-  borderRadius: 12,
   alignItems: 'center',
-  marginTop: 25,
+  borderRadius: 3,
 },
 
 buttonText: {
